0a9a66ff44f00679cb01f4521d19e510fb9bb064
ChenyiXu(Allen) <allen_xu135@163.com>
2023-02-02 20:42:40 +0000

    Initial commit

---
+++ httpserver.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+
+
+class HttpServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path == '/webapps':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(bytes(str(datetime.now()), "utf-8"))
+        else:
+            m = "Unsupported URI: " + self.path
+            self.send_error(400, m)
+
+
+httpd = HTTPServer(('localhost', 10000), HttpServer)
+httpd.serve_forever()
